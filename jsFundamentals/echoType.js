ab28b6c0d3810deada98cb60a2f19a6b2ba67989
StoyanGalchev <galchev98@gmail.com>
2023-03-04 23:30:19 +0200

    Update echoType.js

--- jsFundamentals/echoType.js
+++ jsFundamentals/echoType.js
@@ -1,13 +1,12 @@
 function echoType(arg) {
-  if (typeof arg === "string") {
-    console.log("string");
-    console.log(arg);
-  } else if (typeof arg === "number") {
-    console.log("number");
-    console.log(arg);
-  } else {
-    console.log("object");
-    console.log("Parameter is not suitable for printing");
+    if (typeof arg === "string") {
+      console.log("string");
+      console.log(arg);
+    } else if (typeof arg === "number") {
+      console.log("number");
+      console.log(arg);
+    } else {
+      console.log("object");
+      console.log("Parameter is not suitable for printing");
+    }
   }
-}
-echoType(24);
