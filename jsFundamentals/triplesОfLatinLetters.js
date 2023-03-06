5df353af9a9313cb2af46cd4de6118c7969bf449
StoyanGalchev <galchev98@gmail.com>
2023-03-06 15:48:25 +0200

    Update triplesОfLatinLetters.js

--- jsFundamentals/triplesОfLatinLetters.js
+++ jsFundamentals/triplesОfLatinLetters.js
@@ -1,15 +1,19 @@
-function triplesOfLatinLetters (n){
-    for(let i = 0; i < n; i++){
-        let firstLetter = String.fromCharCode(i + 97);
-        for(let j = 0; j < n; j++){
-            let secondLetter = String.fromCharCode(j + 97);
-            for(let k = 0; k < n; k++){
-                let thirdLetter = String.fromCharCode(k + 97);
-                console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
-            }
-        }
+function triplesOfLatinLetters(n) {
+  for (let i = 0; i < n; i++) {
+    let firstLetter = String.fromCharCode(i + 97);
+    for (let j = 0; j < n; j++) {
+      let secondLetter = String.fromCharCode(j + 97);
+      for (let k = 0; k < n; k++) {
+        let thirdLetter = String.fromCharCode(k + 97);
+        console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+      }
     }
-   
-
+  }
 }
-triplesOfLatinLetters(3)
+
+triplesOfLatinLetters();
+
+word="abcd"
+var u=word[0].toUpperCase()
+
+console.log(word[1].toUpperCase());
