c272595608ef987e226e5f2da6e13a5e28d0f849
StoyanGalchev <galchev98@gmail.com>
2023-03-07 13:28:27 +0200

    Create biggestOf3Numbers.js

---
+++ jsFundamentals/biggestOf3Numbers.js
@@ -0,0 +1,15 @@
+function biggestOf3Numbers(first, second, third) {
+  let biggestNum = Number.MIN_SAFE_INTEGER;
+  if (first > biggestNum) {
+    biggestNum = first;
+  }
+  if (second > biggestNum) {
+    biggestNum = second;
+  }
+  if (third > biggestNum) {
+    biggestNum = third;
+  }
+
+  console.log(biggestNum);
+}
+biggestOf3Numbers(-2, 7, 3);
