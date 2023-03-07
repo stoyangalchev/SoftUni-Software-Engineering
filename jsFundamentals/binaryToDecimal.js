b3cf2989565a9f03a5a964f97e50c18dda8e8e19
StoyanGalchev <galchev98@gmail.com>
2023-03-07 14:13:51 +0200

    Create binaryToDecimal.js

---
+++ jsFundamentals/binaryToDecimal.js
@@ -0,0 +1,20 @@
+function binaryToDecimal(binary) {
+  var binary = binary;
+  var digit = parseInt(binary, 2);
+  console.log(digit);
+}
+binaryToDecimal("11110000");
+
+function binaryToDecimals(numberAsString) {
+  numberAsString = numberAsString.split(""); // Разделяме числото на отделни символи чрез .split("") и го получаваме като масив от символи
+  let decimal = 0;
+  let power = numberAsString.length - 1; // Степента е равна на дължината на стринга - 1
+  for (let i = 0; i < numberAsString.length; i++) {
+    // Въртим по елементите на масива
+    decimal += Number(numberAsString[i]) * Math.pow(2, power); // Взимаме числото от масива и го преобръщаме към число, след което го умноаважаме по 2 на степента
+    power--; // Намаляме степента всеки път с единица.
+  }
+  console.log(decimal);
+}
+
+binaryToDecimal("11110000");
