a6b8f85b9eb0515d6c9a261ad488abd3c75a8400
StoyanGalchev <galchev98@gmail.com>
2023-03-08 20:49:52 +0200

    Create dayOfWeek.js

---
+++ jsFundamentals/dayOfWeek.js
@@ -0,0 +1,13 @@
+function dayOfWeek (num){
+switch(num){
+    case 1:console.log("Monday");break;
+    case 2:console.log("Tuesday");break;
+    case 3:console.log("Wednesday");break;
+    case 4:console.log("Thursday");break;
+    case 5:console.log("Friday");break;
+    case 6:console.log("Saturday");break;
+    case 7:console.log("Sunday");break;
+     default:console.log("Invalid day!");
+}
+}
+dayOfWeek(11)
