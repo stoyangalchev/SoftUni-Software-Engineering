058f99ac2d4f0d127b9f733e0e9a0bf3cac4b046
StoyanGalchev <galchev98@gmail.com>
2023-03-21 16:20:15 +0200

    Create oddAndEvenSum.js

---
+++ jsFundamentals/oddAndEvenSum.js
@@ -0,0 +1,16 @@
+function oddAndEvenSum(number) {
+  let asString = String(number);
+  let odd = 0;
+  let even = 0;
+
+  for (let el of asString) {
+    let curent = Number(el);
+    if (curent % 2 == 0) {
+      even += curent;
+    } else {
+      odd += curent;
+    }
+  }
+  console.log(`Odd sum = ${odd}, Even sum = ${even}`);
+}
+oddAndEvenSum(1000435);
