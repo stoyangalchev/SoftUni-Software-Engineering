2021f5bad2612bd29ba79389d413c4377908872a
StoyanGalchev <galchev98@gmail.com>
2023-03-22 10:51:56 +0200

    Create factorialDivision.js

---
+++ jsFundamentals/factorialDivision.js
@@ -0,0 +1,22 @@
+function factorialDivision(a, b) {
+  let firstNum = Number(a);
+  let secondNum = Number(b);
+  let firstF = factorial(firstNum);
+  let secondF = factorial(secondNum);
+  let difference = divideNums(firstF, secondF);
+  console.log(difference.toFixed(2));
+
+  function factorial(num) {
+    let result = 1;
+    for (let i = 1; i <= num; i++) {
+      result *= i;
+    }
+    return result;
+  }
+  function divideNums(firstF, secondF) {
+    let result = firstF / secondF;
+    return result;
+  }
+}
+
+factorialDivision(5, 2);
