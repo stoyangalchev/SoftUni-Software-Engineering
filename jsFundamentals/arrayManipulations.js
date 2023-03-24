8c23bc9b35e280016811874bfda718fa76f313a2
StoyanGalchev <galchev98@gmail.com>
2023-03-24 20:34:11 +0200

    Update arrayManipulations.js

--- jsFundamentals/arrayManipulations.js
+++ jsFundamentals/arrayManipulations.js
@@ -1,37 +1,37 @@
 function arrayManipulations(commands) {
-    let arr = commands.shift().split(' ').map(Number);
-    for (let i = 0; i < commands.length; i++) {
-        let [command, firstNum, secondNum] = commands[i].split(' ');
-        firstNum = Number(firstNum);
-        secondNum = Number(secondNum);
-        switch (command) {
-            case 'Add':
-                add(firstNum);
-                break;
-            case 'Remove':
-                remove(firstNum);
-                break;
-            case 'RemoveAt':
-                removeAt(firstNum);
-                break;
-            case 'Insert':
-                insert(firstNum, secondNum);
-                break;
-        }
+  let arr = commands.shift().split(" ").map(Number);
+  for (let i = 0; i < commands.length; i++) {
+    let [command, firstNum, secondNum] = commands[i].split(" ");
+    firstNum = Number(firstNum);
+    secondNum = Number(secondNum);
+    switch (command) {
+      case "Add":
+        add(firstNum);
+        break;
+      case "Remove":
+        remove(firstNum);
+        break;
+      case "RemoveAt":
+        removeAt(firstNum);
+        break;
+      case "Insert":
+        insert(firstNum, secondNum);
+        break;
     }
-    function add(el) {
-        arr.push(el);
-    }
-    function remove(num) {
-        arr = arr.filter(el => el !== num);
-    }
-    function removeAt(index) {
-        arr.splice(index, 1);
-    }
-    function insert(num, index) {
-        arr.splice(index, 0, num);
-    }
-    console.log(arr.join(' '));
+  }
+  function add(el) {
+    arr.push(el);
+  }
+  function remove(num) {
+    arr = arr.filter((el) => el !== num);
+  }
+  function removeAt(index) {
+    arr.splice(index, 1);
+  }
+  function insert(num, index) {
+    arr.splice(index, 0, num);
+  }
+  console.log(arr.join(" "));
 }
 arrayManipulations([
   "4 19 2 53 6 43",
