28778349101ed6ec05110a56d169b16601215cd6
StoyanGalchev <galchev98@gmail.com>
2023-04-01 00:24:15 +0300

    Update convertToJSON.js

--- jsFundamentals/convertToJSON.js
+++ jsFundamentals/convertToJSON.js
@@ -1,10 +1,10 @@
-function convertToJSON (firstName,lastName,hairColor){
-let object= {
-    name:firstName,
-    lastName:lastName,
-    hairColor:hairColor
+function convertToJSON(firstName, lastName, hairColor) {
+  let object = {
+    name: firstName,
+    lastName: lastName,
+    hairColor: hairColor,
+  };
+  let toJSONSTRING = JSON.stringify(object);
+  console.log(toJSONSTRING);
 }
-let toJSONSTRING=JSON.stringify(object);
-console.log(toJSONSTRING);
-}
-convertToJSON('George', 'Jones', 'Brown')
+convertToJSON("George", "Jones", "Brown");
