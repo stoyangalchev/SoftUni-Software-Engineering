a6aa37f164589e54a7e81951d457587e8036320f
StoyanGalchev <galchev98@gmail.com>
2023-04-02 22:26:17 +0300

    Create towns.js

---
+++ jsFundamentals/towns.js
@@ -0,0 +1,15 @@
+function towns(input) {
+  let object = {};
+
+  for (let i = 0; i < input.length; i++) {
+    let splitedEL = input[i].split(" | ");
+    let town = splitedEL[0];
+    let latitude = Number(splitedEL[1]).toFixed(2);
+    let longtitude = Number(splitedEL[2]).toFixed(2);
+    object.town = town;
+    object.latitude = latitude;
+    object.longtitude = longtitude;
+    console.log(object);
+  }
+}
+towns(["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"]);
