750948aaee19267e314e4786e644b89d0c3bfe0e
StoyanGalchev <galchev98@gmail.com>
2023-04-06 21:06:49 +0300

    Create classVehicle.js

---
+++ jsFundamentals/classVehicle.js
@@ -0,0 +1,20 @@
+class Vehicle{
+    constructor (type,model,parts,fuel,drive){
+        this.type=type,
+        this.model=model,
+        this.parts=parts,
+        this.fuel=fuel,
+        this.drive=drive
+    }
+
+    
+}
+
+
+
+    let parts = { engine: 6, power: 100 };
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+    vehicle.drive(100);
+    console.log(vehicle.fuel);
+    console.log(vehicle.parts.quality);
+    
