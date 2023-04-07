120a2ef9eaf84510054d9def288647d8b018361a
StoyanGalchev <galchev98@gmail.com>
2023-04-07 13:37:21 +0300

    Update flightSchedule.js

--- jsFundamentals/flightSchedule.js
+++ jsFundamentals/flightSchedule.js
@@ -1,32 +1,27 @@
 function flightSchedule(input) {
- 
-    let firstArr = input.shift();
-    let secondArr = input.shift();
-    let thirdArr = input.shift();
-    let flights = {};
- 
-    for (const line of firstArr) {
-        let [number, Destination] = line.split(' ');
-        flights[number] = {Destination, Status: 'Ready to fly'}
+  let firstArr = input.shift();
+  let secondArr = input.shift();
+  let thirdArr = input.shift();
+  let flights = {};
+
+  for (const line of firstArr) {
+    let [number, Destination] = line.split(" ");
+    flights[number] = { Destination, Status: "Ready to fly" };
+  }
+
+  for (const line of secondArr) {
+    let [number, currentStatus] = line.split(" ");
+
+    if (flights.hasOwnProperty(number)) {
+      flights[number].Status = currentStatus;
     }
-    
- 
-    for (const line of secondArr) {
-        let [number, currentStatus] = line.split(' ');
- 
-        if (flights.hasOwnProperty(number)) {
-            flights[number].Status = currentStatus;
-        }
+  }
+
+  for (const line in flights) {
+    if (flights[line].Status == thirdArr) {
+      console.log(flights[line]);
     }
-  
- 
-    for (const line in flights) {
-        if (flights[line].Status == thirdArr) {
-            console.log(flights[line])
-  
-        }
-    }  
- 
+  }
 }
 flightSchedule([
   [
