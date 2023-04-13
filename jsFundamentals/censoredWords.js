818fd4404b2bea0cd1678386c947bff682228ffa
Stoyan Galchev <galchev98@gmail.com>
2023-04-13 17:52:07 +0300

    Create censoredWords.js

---
+++ jsFundamentals/censoredWords.js
@@ -0,0 +1,9 @@
+function censoredWords(str, word) {
+    while (str.includes(word)) {
+      str = str.replace(word, '*'.repeat(word.length))
+    }
+    console.log(str);
+}
+censoredWords('A small sentence with some words',
+
+'small')
