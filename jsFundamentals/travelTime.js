dd0b5f5e1806ae79a6d1cee48ee1f997eb8c8b15
Stoyan Galchev <galchev98@gmail.com>
2023-04-13 09:41:14 +0300

    Create travelTime.js

---
+++ jsFundamentals/travelTime.js
@@ -0,0 +1,41 @@
+function solve(input) {
+  let result = {};
+
+  input.map((x) => {
+    let [country, town, price] = x.split(" > ");
+
+    // if (x[country] === undefined) {
+    if (!result.hasOwnProperty(country)) {
+      // x[country] = {};
+      result[country] = {};
+    }
+
+    if (!result[country].hasOwnProperty(town)) {
+      result[country][town] = Number(price);
+    } else if (result[country][town] > Number(price)) {
+      result[country][town] = Number(price);
+    }
+
+   
+  });
+
+  let sorted = Object.entries(result).sort((a, b) => a[0].localeCompare(b[0]));
+  sorted.forEach(([country, towns]) => {
+    // console.log(`${x[0]} -> ${x[1]} -> ${x[2]}`)
+    let finalString = `${country} -> `;
+    let sortedTowns = Object.entries(towns).sort((a, b) => a[1] - b[1]);
+    sortedTowns.forEach(([town, price]) => {
+      finalString += `${town} -> ${price} `;
+    });
+
+    console.log(finalString.trim());
+  });
+}
+solve([
+  "Bulgaria > Sofia > 25000",
+  "aaa > Sofia > 1",
+  "aa > Orgrimar > 0",
+  "Albania > Tirana > 25000",
+  "zz > Aarna > 25010",
+  "Bulgaria > Lukovit > 10",
+]);
