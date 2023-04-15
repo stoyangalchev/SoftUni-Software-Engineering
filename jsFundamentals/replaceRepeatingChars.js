7002516a2775a607e0a4384a4fa3ea52f29f06a3
Stoyan Galchev <galchev98@gmail.com>
2023-04-16 00:41:45 +0300

    Create replaceRepeatingChars.js

---
+++ jsFundamentals/replaceRepeatingChars.js
@@ -0,0 +1,13 @@
+function replaceRepeatingChars(input) {
+  let arr = [];
+  for (let i = 0; i < input.length; i++) {
+    if (input[i] !== input[i - 1]) {
+      arr.push(input[i]);
+    }
+  }
+  console.log(arr.join(""));
+}
+replaceRepeatingChars("qqqwerqwecccwd");
+if ("qwerqwecwd"!=="qwerqwecwd"){
+    console.log(true);
+}
