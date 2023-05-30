7a3583faf2a2ae77a0f46ba48900c48a28f7f032
Stoyan Galchev <galchev98@gmail.com>
2023-05-30 12:39:47 +0300

    Create binary.js

---
+++ jsFundamentals/binary.js
@@ -0,0 +1,7 @@
+function addBinary(a,b){
+  return (a+b).toString(2)
+}
+
+console.log(addBinary(1, 2)); //Should be 11
+console.log(addBinary(51, 12)); //Should be 111111
+console.log(addBinary(100, 0)); //Should be 1100100
