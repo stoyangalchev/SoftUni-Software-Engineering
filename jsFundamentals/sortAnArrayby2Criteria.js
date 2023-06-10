83d2d0f5e232288258a0739e488a9898b3d4fae0
Stoyan Galchev <galchev98@gmail.com>
2023-06-10 18:26:49 +0300

    Update sortAnArrayby2Criteria

--- jsFundamentals/sortAnArrayby2Criteria.js
+++ jsFundamentals/sortAnArrayby2Criteria.js
@@ -1,7 +1,21 @@
 function sortAnArrayby2Criteria(arr) {
   const twoCriteriaSort = (a, b) => a.length - b.length || a.localeCompare(b);
-  arr.sort(twoCriteriaSort);
+  arr.sort();
+
   console.log(arr.join("\n"));
 }
 
-sortAnArrayby2Criteria(["test", "Deny", "omen", "Default"]);
+sortAnArrayby2Criteria(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
+function solve(input) {
+  let sortedByLength = (x) => {
+    return x.sort((a, b) => a.length - b.length);
+  };
+
+  function sortedByAlfa(x) {
+    return x.sort((a, b) => a.localeCompare(b));
+  }
+
+  let result = sortedByLength(sortedByAlfa(input));
+  return result.join("\n");
+}
+console.log(solve(["test", "Deny", "omen", "Default"]));
