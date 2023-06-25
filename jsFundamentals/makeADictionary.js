6509e31e4ca596a15366034efcbc916f201a7e72
Stoyan Galchev <galchev98@gmail.com>
2023-06-25 16:59:14 +0300

    Update makeADictionary.js

--- jsFundamentals/makeADictionary.js
+++ jsFundamentals/makeADictionary.js
@@ -26,3 +26,35 @@ makeADictionary([
 
   '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
 ]);
+function solve2(input) {
+  let sortable = [];
+  for (let i = 0; i < input.length; i++) {
+    let line = input[i];
+    let parsedLine = JSON.parse(line);
+
+    for (let [key, value] of Object.entries(parsedLine)) {
+      sortable[key] = value;
+    }
+  }
+  const ordered = Object.keys(sortable)
+    .sort()
+    .reduce((obj, key) => {
+      obj[key] = sortable[key];
+      return obj;
+    }, {});
+
+  for (let [key, value] of Object.entries(ordered)) {
+    console.log(`Term: ${key} => Definition: ${value}`);
+  }
+}
+solve2([
+  '{"Coffee":"A hot drink made from the roasted and ground seeds (coffee beans) of a tropical shrub."}',
+
+  '{"Bus":"A large motor vehicle carrying passengers by road, typically one serving the public on a fixed route and for a fare."}',
+
+  '{"Boiler":"A fuel-burning apparatus or container for heating water."}',
+
+  '{"Tape":"A narrow strip of material, typically used to hold or fasten something."}',
+
+  '{"Microphone":"An instrument for converting sound waves into electrical energy variations which may then be amplified, transmitted, or recorded."}',
+]);
