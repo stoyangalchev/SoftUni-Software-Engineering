6b00d6451284bd4ff472048a7f6040265ea2b3e5
Stoyan Galchev <galchev98@gmail.com>
2023-07-10 15:46:35 +0300

    Update pascalCaseSplitter.js

--- jsFundamentals/pascalCaseSplitter.js
+++ jsFundamentals/pascalCaseSplitter.js
@@ -11,3 +11,28 @@ function pascalCaseSplitter(input) {
   console.log(text.substring(1).split(" ").join(", "));
 }
 pascalCaseSplitter("SplitMeIfYouCanHaHaYouCantOrYouCan");
+
+function solve(input) {
+  let splitInput = input.split("");
+  let saverWord = "";
+
+  let length = splitInput.length;
+
+  for (let i = 0; i < length; i++) {
+    let firstChar = splitInput[i];
+    let secondChar = splitInput[i + 1];
+
+    if (firstChar.charCodeAt() >= 65 && firstChar.charCodeAt() <= 90) {
+      saverWord += ` ${firstChar}`;
+    } else if (firstChar.charCodeAt() >= 97 && firstChar.charCodeAt() <= 122) {
+      saverWord += `${firstChar}`;
+    } else if (
+      secondChar.charCodeAt() >= 97 &&
+      secondChar.charCodeAt() <= 122
+    ) {
+      saverWord += secondChar;
+    }
+  }
+  console.log(saverWord.trim().split(" ").join(", "));
+}
+solve("HolSreDor");
