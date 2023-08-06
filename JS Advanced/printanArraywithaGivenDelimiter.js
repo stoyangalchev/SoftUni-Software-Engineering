759c7aff3b8fbeb43736c34e97396c0c7c3d64a3
Stoyan Galchev <galchev98@gmail.com>
2023-08-06 22:45:21 +0300

    Create printanArraywithaGivenDelimiter.js

---
+++ JS Advanced/printanArraywithaGivenDelimiter.js
@@ -0,0 +1,7 @@
+function printanArraywithaGivenDelimiter(arr, operator) {
+  console.log(arr.join(operator));
+}
+printanArraywithaGivenDelimiter(
+  ["How about no?", "I", "will", "not", "do", "it!"],
+  "_"
+);
