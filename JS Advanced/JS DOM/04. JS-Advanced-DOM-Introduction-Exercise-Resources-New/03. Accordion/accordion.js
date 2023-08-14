316bb0e6b31d5ddd4ed20cecf330a4a63e2cde52
Stoyan Galchev <galchev98@gmail.com>
2023-08-14 22:30:42 +0300

    Update accordion.js

--- JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+  let buttonElement = document.getElementsByClassName("button")[0];
+
+  let idExtraElement = document.querySelector("#extra");
+
+  if (buttonElement.textContent == "More") {
+    if ((idExtraElement.style.display = "none")) {
+      idExtraElement.style.display = "block";
+      buttonElement.textContent = "Less";
+    }
+  } else if (buttonElement.textContent == "Less") {
+    idExtraElement.style.display = "none";
+    buttonElement.textContent = "More";
+  }
+
+  console.log(buttonElement.textContent);
 }
