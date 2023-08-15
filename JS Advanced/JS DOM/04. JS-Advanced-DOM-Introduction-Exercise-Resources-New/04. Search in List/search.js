dc55481d808467778baac3cd6f17c8f03d4067b8
Stoyan Galchev <galchev98@gmail.com>
2023-08-15 14:08:08 +0300

    Update search.js

--- JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,21 @@
 function search() {
-   // TODO
+  let inputElement = document.getElementById("searchText").value;
+  let townsElement = Array.from(document.querySelectorAll("ul li"));
+  let match = document.getElementById("result");
+  let count = 0;
+
+  for (let town of townsElement) {
+    if (inputElement == "") {
+      break;
+    }
+    if (town.textContent.includes(inputElement)) {
+      town.style.fontWeight = "bold";
+      town.style.textDecoration = "underline";
+      count++;
+    } else {
+      town.style.fontWeight = "none";
+      town.style.textDecoration = "none";
+    }
+  }
+  match.textContent = `${count} matches found`;
 }
