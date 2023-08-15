492c23619c4e08d41440271d4e1891cb9d6b4579
Stoyan Galchev <galchev98@gmail.com>
2023-08-15 17:16:04 +0300

    Dom

--- JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  document.querySelector("#searchBtn").addEventListener("click", onClick);
+  let inputField = document.getElementById("searchField");
+  let tableRow = document.querySelectorAll("tbody tr");
 
-   function onClick() {
-      //   TODO:
-
-   }
+  if (inputField === null || tableRow === null) {
+    throw new Error("Missing HTML elements!");
+  }
+  function onClick() {
+    for (let tr of tableRow) {
+      tr.classList.remove("select");
+    }
+    if (inputField.value == "") {
+      return "";
+    }
+    for (let tr of tableRow) {
+      if (tr.textContent.includes(inputField.value)) {
+        tr.classList.add("select");
+      }
+    }
+    inputField.value = "";
+  }
 }
--- JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ JS Advanced/JS DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,27 @@
 function solve() {
-  //TODO
+  let text = document
+    .getElementById("input")
+    .value.split(".")
+    .filter((e) => e);
+  let output = [];
+
+  for (i = 0; i < text.length; i++) {
+    let sentence = text[i];
+    if (sentence.length > 0) {
+      if (i % 3 === 0 && i != text.length - 1) {
+        output.push("<p>" + sentence + ".");
+      } else if (i % 3 === 1 && i != text.length - 1) {
+        output.push(sentence + ".");
+      } else if (i % 3 === 2 && i != text.length - 1) {
+        output.push(sentence + "." + "</p>");
+      } else {
+        if (i % 3 === 0 && i === text.length - 1) {
+          output.push("<p>" + sentence + "." + "</p>");
+        } else {
+          output.push(sentence + "." + "</p>");
+        }
+      }
+    }
+  }
+  document.getElementById("output").innerHTML = output.join(" ");
 }
