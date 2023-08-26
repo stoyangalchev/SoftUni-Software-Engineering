29aa0b4fee142a7f7d5b4bba970b10897db72fe3
Stoyan Galchev <galchev98@gmail.com>
2023-08-26 17:38:08 +0300

    Create fibonacci.js

---
+++ JS Advanced/fibonacci.js
@@ -0,0 +1,18 @@
+function getFibonator() {
+  let x = 1;
+  let y = 1;
+  let z = 0;
+  return function getNext() {
+    [z, x, y] = [x, y, x + y];
+    return z;
+  };
+}
+
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
