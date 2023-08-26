c7cbb79ebe0307f8a6b02624c1123056c52eed18
Stoyan Galchev <galchev98@gmail.com>
2023-08-26 17:40:37 +0300

    Update solve.js

--- JS Advanced/solve.js
+++ JS Advanced/solve.js
@@ -1,12 +1,4 @@
-function* generator() {
-    yield 1;
-    yield 2;
-    yield 3;
-  }
-  
-  const gen = generator(); // "Generator { }"
-  
-  console.log(gen.next()); // 1
-  console.log(gen.next()); // 2
-  console.log(gen.next()); // 3
-  console.log(gen.next());
+function  generator() {
+    console.log(2);
+}
+generator()
