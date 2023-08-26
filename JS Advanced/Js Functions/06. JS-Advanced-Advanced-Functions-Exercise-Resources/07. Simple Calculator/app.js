720fdde737f64d9a149b89ede11f17266fcb2115
Stoyan Galchev <galchev98@gmail.com>
2023-08-26 17:38:19 +0300

    Update app.js

--- JS Advanced/Js Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
+++ JS Advanced/Js Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/07. Simple Calculator/app.js
@@ -1,7 +1,22 @@
 function calculator() {
-    // TODO:
+  function init(selector1, selector2, resultSelector) {
+    inpu1 = document.querySelector(selector1);
+    input2 = document.querySelector(selector2);
+    result = document.querySelector(resultSelector);
+  }
+  function add() {
+    result.value = Number(inpu1.value) + Number(input2.value);
+  }
+  function subtract() {
+    result.value = Number(inpu1.value) - Number(input2.value);
+  }
+
+  return {
+    init,
+    add,
+    subtract,
+  };
 }
 
-
-
-
+const calculate = calculator();
+calculate.init("#num1", "#num2", "#result");
