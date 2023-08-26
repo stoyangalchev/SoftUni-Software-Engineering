17ce52a7cec6cb60858c5a597a55906ebffa9b7b
Stoyan Galchev <galchev98@gmail.com>
2023-08-26 17:41:03 +0300

    Update script.js

--- JS Advanced/Js Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/08. Next Article/script.js
+++ JS Advanced/Js Functions/06. JS-Advanced-Advanced-Functions-Exercise-Resources/08. Next Article/script.js
@@ -1,3 +1,12 @@
 function getArticleGenerator(articles) {
-    // TODO
+  let input = articles;
+  let getDivElement = document.getElementById("content");
+
+  return function showNext() {
+    if (input.length > 0) {
+      let createElement = document.createElement("article");
+      createElement.innerText = input.shift();
+      getDivElement.appendChild(createElement);
+    }
+  };
 }
