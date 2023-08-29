5ff324a263af263e4bfd692abb723968df7d536f
Stoyan Galchev <galchev98@gmail.com>
2023-08-29 21:07:02 +0300

    Update app.js

--- JS Advanced/Unit  Testing and Error Handling/JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
+++ JS Advanced/Unit  Testing and Error Handling/JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/06.Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    // TODO
+  let idElement = document.getElementById("email");
+
+  idElement.addEventListener("change", (e) => {
+    let regex = /^[a-z]+\@[a-z]+\.[a-z]+$/gm;
+    if (regex.test(e.target.value)) {
+      e.currentTarget.classList.remove("error");
+    } else {
+      e.currentTarget.classList.add("error");
+    }
+  });
 }
