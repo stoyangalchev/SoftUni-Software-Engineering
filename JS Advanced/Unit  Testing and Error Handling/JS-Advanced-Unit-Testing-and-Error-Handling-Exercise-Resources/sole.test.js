971f9bebe50e3abb426cea83e25e6e1833ca1b28
Stoyan Galchev <galchev98@gmail.com>
2023-08-29 21:06:49 +0300

    Delete sole.test.js

--- JS Advanced/Unit  Testing and Error Handling/JS-Advanced-Unit-Testing-and-Error-Handling-Exercise-Resources/sole.test.js
+++
@@ -1,9 +0,0 @@
-const solve = require("./sole");
-const expect = require("chai").expect;
-
-describe("test", () => {
-  it("2safsafas", () => {
-    let result = solve(2);
-    expect(result).to.equal(2);
-  });
-});
