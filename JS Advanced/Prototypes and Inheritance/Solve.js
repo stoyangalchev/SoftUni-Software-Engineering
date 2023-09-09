edcc582e0fabb80c140ce4ad012bfffe4fe1538c
Stoyan Galchev <galchev98@gmail.com>
2023-09-09 11:54:18 +0300

    Create Solve.js

---
+++ JS Advanced/Prototypes and Inheritance/Solve.js
@@ -0,0 +1,16 @@
+function Product(name, price) {
+  this.name = name;
+  this.price = price;
+  
+}
+
+function Food(name, price) {
+   
+  Product.call(this, name, price);
+
+  this.category = "food";
+}
+
+
+console.log(new Food("cheese", 5).name);
+// Expected output: "cheese"
