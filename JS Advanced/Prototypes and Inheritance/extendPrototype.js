63900946390f519924e4504cb2765285fd099d23
Stoyan Galchev <galchev98@gmail.com>
2023-09-22 13:24:36 +0300

    Update extendPrototype.js

--- JS Advanced/Prototypes and Inheritance/extendPrototype.js
+++ JS Advanced/Prototypes and Inheritance/extendPrototype.js
@@ -1,18 +1,18 @@
 function extendPrototype(classToExtend) {
-    classToExtend.prototype.species = 'Human';
-    classToExtend.prototype.toSpeciesString = function (){
-        return `I am a ${this.species}. ${this.toString()}`;
-    }
+  classToExtend.prototype.species = "Human";
+  classToExtend.prototype.toSpeciesString = function () {
+    return `I am a ${this.species}. ${this.toString()}`;
+  };
 }
 
 function Person(name, email) {
-    this.name = name;
-    this.email = email;
+  this.name = name;
+  this.email = email;
 }
 
 Person.prototype.toString = function () {
-    return `Person (name: ${this.name}, email: ${this.email})`;
-}
+  return `Person (name: ${this.name}, email: ${this.email})`;
+};
 
 extendPrototype(Person);
 let person = new Person("Ta4o", "TA4OWEB@test.com");
