7c78fed84ab3b01084bff12b9e5c75fcf7db77d4
Stoyan Galchev <galchev98@gmail.com>
2023-11-13 17:08:41 +0200

    Update app.js

--- JS Application/Architecture and Testing /03.SoftTerest/src/app.js
+++ JS Application/Architecture and Testing /03.SoftTerest/src/app.js
@@ -1,26 +1,30 @@
-async function request(url, method = "get", data) {
-  const options = {
-    method,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  };
-  try {
-    const response = await fetch(url, options);
+import { logout } from "./api/users.js";
+import { intitialize } from "./router.js";
+import { showCatalog } from "./views/catalog.js";
+import { showCreate } from "./views/create.js";
+import { showDetails } from "./views/details.js";
+import { showHome } from "./views/home.js";
+import { showLogin } from "./views/login.js";
+import { showRegister } from "./views/register.js";
 
-    if (response.ok !== true) {
-      throw new Error(response.statusText);
-    }
-    if (response.status === 204) {
-      return response;
-    } else {
-      return response.json();
-    }
-  } catch (eror) {
-    alert(eror.message);
-    throw eror;
-  }
-}
-window.api = {
-  request,
+document.getElementById("views").remove();
+
+const links = {
+  "/": showHome,
+  "/catalog": showCatalog,
+  "/login": showLogin,
+  "/register": showRegister,
+  "/details": showDetails,
+  "/create": showCreate,
+  "/logout": onLogOut,
 };
+
+const router = intitialize(links);
+router.updateNav();
+router.goTo("/");
+
+function onLogOut() {
+  logout();
+  router.updateNav();
+  router.goTo("/");
+}
