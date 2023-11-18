17648a35b365a3a919fcbd62da88c44bd0475312
Stoyan Galchev <galchev98@gmail.com>
2023-11-18 10:41:47 +0200

    Create contact.js

---
+++ JS Application/Client Side Rendering/Problem 1/src/templates/contact.js
@@ -0,0 +1,12 @@
+const contactTemplate = (contact) => `
+    <div class="card" style="width: 18rem;">
+        <img src="https://www1.nyc.gov/assets/nycha/images/content/pages/contact-in-person.png" class="card-img-top" alt="contact">
+        <div class="card-body">
+            <h5 class="card-title">${contact.person}</h5>
+            <p class="card-text">${contact.phone}</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
+`;
+
+export default contactTemplate;
