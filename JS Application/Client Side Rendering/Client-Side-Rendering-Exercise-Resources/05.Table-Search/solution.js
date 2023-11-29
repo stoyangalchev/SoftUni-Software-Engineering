5c1dd3568ad164d57560799def2401fc39486cb4
Stoyan Galchev <galchev98@gmail.com>
2023-11-29 20:17:01 +0200

    Update solution.js

--- JS Application/Client Side Rendering/Client-Side-Rendering-Exercise-Resources/05.Table-Search/solution.js
+++ JS Application/Client Side Rendering/Client-Side-Rendering-Exercise-Resources/05.Table-Search/solution.js
@@ -1,8 +1,42 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+  const url = `http://localhost:3030/jsonstore/advanced/table`;
+  const body = document.querySelector(`tbody`);
+  const input = document.querySelector(`#searchField`);
+  const searchBtn = document.querySelector(`#searchBtn`);
 
-   function onClick() {
-      //   TODO:
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      Object.values(data).forEach((obj) => {
+        const row = document.createElement("tr");
+        const name = document.createElement("td");
+        const email = document.createElement("td");
+        const course = document.createElement("td");
 
-   }
+        name.textContent = obj.firstName + " " + obj.lastName;
+        email.textContent = obj.email;
+        course.textContent = obj.course;
+
+        row.appendChild(name);
+        row.appendChild(email);
+        row.appendChild(course);
+        body.appendChild(row);
+      });
+    });
+  searchBtn.addEventListener("click", onClick);
+
+  function onClick() {
+    const rows = Array.from(body.querySelectorAll("tr"));
+
+    const search = input.value.toLowerCase();
+    rows.forEach((r) => {
+      if (r.textContent.toLowerCase().includes(search)) {
+        r.classList.add("select");
+      } else {
+        r.classList.remove("select");
+      }
+    });
+    input.value = "";
+  }
 }
+solve();
