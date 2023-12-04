f324f49541bb982d9e72200e7a280c38f3563e6f
Stoyan Galchev <galchev98@gmail.com>
2023-12-04 20:12:19 +0200

    Create renderEdit.js

---
+++ JS Application/Single Page Application/Movies SPA/src/renderEdit.js
@@ -0,0 +1,47 @@
+import { details } from "./details.js";
+import { getRequest } from "./getRequest.js";
+import { putRequest } from "./putRequest.js";
+
+const editMovieSection = document.querySelector('#edit-movie');
+editMovieSection.remove();
+const body = document.querySelector('body');
+let movieId = '';
+const formElement = editMovieSection.querySelector('form');
+
+
+export function renderEdit(event) {
+    const divWithDataset = event.target.parentElement;
+    movieId = divWithDataset.dataset.movieId;
+
+    body.querySelector('section').replaceWith(editMovieSection);
+    loadDataForEdit();
+}
+
+async function loadDataForEdit() {
+    const data = await getRequest(`http://localhost:3030/data/movies/${movieId}`);
+    const title = formElement.querySelector('#title');
+    const description = formElement.querySelector('textarea[name="description"]');
+    const img = formElement.querySelector('#imageUrl');
+
+    title.value = data.title;
+    description.value = data.description;
+    img.value = data.img;
+}
+
+formElement.addEventListener('submit', edit);
+
+async function edit(event) {
+    event.preventDefault();
+
+    const form = new FormData(event.currentTarget);
+    const title = form.get('title');
+    const description = form.get('description');
+    const img = form.get('img');
+
+    const dataForRequest = { title, description, img };
+
+    const data = await putRequest(`http://localhost:3030/data/movies/${movieId}`, dataForRequest);
+    details(data)
+    formElement.reset();
+}
+
