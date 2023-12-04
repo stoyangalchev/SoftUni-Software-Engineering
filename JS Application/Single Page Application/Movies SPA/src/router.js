e65adb335ddc98716efbf4095529c809685b3ea0
Stoyan Galchev <galchev98@gmail.com>
2023-12-04 20:12:23 +0200

    Create router.js

---
+++ JS Application/Single Page Application/Movies SPA/src/router.js
@@ -0,0 +1,27 @@
+import { addLike } from './addLike.js';
+import { renderAddMoviePage } from './addMovie.js';
+import { onDelete } from './delete.js';
+import { renderHome } from './home.js';
+import { renderLogin } from './login.js';
+import { logout } from './logout.js';
+import { navHandler } from './navigationHandler.js';
+import { renderRegister } from './register.js';
+import { renderEdit } from './renderEdit.js';
+
+
+const routerData = {
+    '/': renderHome,
+    '/logout': logout,
+    '/login': renderLogin,
+    '/register': renderRegister,
+    '/add': renderAddMoviePage,
+    '/delete': onDelete,
+    '/edit': renderEdit,
+    '/like': addLike,
+}
+
+export function router(pathname, event) {
+   
+    routerData[pathname](event);
+    navHandler();
+}
