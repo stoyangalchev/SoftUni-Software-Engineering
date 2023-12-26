052d24473ea106093d09ec0bf8e62cfd9e425941
Stoyan Galchev <galchev98@gmail.com>
2023-12-26 09:27:14 +0200

    Create accessoryController.js

---
+++ Front-End/RUBIC CUB -Workshop-ExpressJS and Templating/src/controllers/accessoryController.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+
+const accessoryManager = require("../managers/accessoryManager");
+
+router.get("/create", (req, res) => {
+  res.render("accessory/create");
+});
+
+router.post("/create", async (req, res) => {
+  const { name, description, imageUrl } = req.body;
+
+  await accessoryManager.create({ name, description, imageUrl });
+
+  res.redirect("/");
+});
+
+module.exports = router;
+
