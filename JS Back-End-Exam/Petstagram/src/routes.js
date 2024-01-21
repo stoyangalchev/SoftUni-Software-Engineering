8357e87afa0ae6fc7d465b49755e938e482deb40
Stoyan Galchev <galchev98@gmail.com>
2024-01-21 14:08:45 +0200

    Delete component

--- JS Back-End-Exam/Petstagram/src/routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-const homeController = require("./controllers/homeController");
-const userController = require("./controllers/userController");
-
-router.use(homeController);
-router.use("/users", userController);
-
-module.exports = router;
