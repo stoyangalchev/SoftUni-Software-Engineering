31ed4a7b98d587482ba556ceaf3bb0128e6021bb
Stoyan Galchev <galchev98@gmail.com>
2024-01-22 20:54:38 +0200

    Create creaturesServices.js

---
+++ JS Back-End-Exam/SEARCH_Gaming-Team/src/services/creaturesServices.js
@@ -0,0 +1,55 @@
+const { log } = require("util");
+const Creatures = require("../models/Creatures");
+const User = require("../models/User");
+
+//Gives all the creatures.
+exports.getAll = () => Creatures.find().populate("owner").lean();
+
+//Gives the creature by id.
+exports.getOne = (creaturesId) =>
+  Creatures.findById(creaturesId).populate("owner").lean(); //
+
+//Creates a new creature.
+exports.create = (creaturesData) => Creatures.create(creaturesData);
+
+//Get last three creatures added.
+exports.getLastThree = () =>
+  Creatures.find({}).sort({ createdAt: -1 }).limit(3).lean();
+
+//Deletes the creature.
+exports.delete = (creaturesId) => Creatures.findByIdAndDelete(creaturesId);
+
+//Gives the creature a new data.
+exports.updateOne = (creaturesId, creaturesData) =>
+  Creatures.findByIdAndUpdate(creaturesId, creaturesData);
+
+//Gives the creature a comment.
+exports.buy = async (creaturesId, commentData) => {
+  const creatures = await Creatures.findById(creaturesId);
+  creatures.donations.push(commentData);
+  return creatures.save();
+};
+
+//Gives the owner of the creature.
+exports.search = (creaturesName, creaturesPlatform) => {
+  if (creaturesName) {
+    return Creatures.find({
+      name: { $regex: creaturesName, $options: "i" },
+    }).lean();
+  }
+  console.log(creaturesPlatform);
+  if(creaturesPlatform){
+    return Creatures.find({
+      platform: { $regex: creaturesPlatform, $options: "i" },
+    }).lean();
+  }
+  // if (creaturesType) {
+  //   return Creatures.find({
+  //     type: { $regex: creaturesType, $options: "i" },
+  //   }).lean();
+  // }
+
+  // if (!creaturesName && creaturesType) {
+  //   return Creatures.find({ type: creaturesType }).lean();
+  // }
+};
