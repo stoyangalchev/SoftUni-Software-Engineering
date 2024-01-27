d5fc0b541edfd405beb816b8b9780c7a46ad22d2
stoyangalchev <galchev98@gmail.com>
2024-01-27 21:22:19 +0200

    Create expressConfig.js

---
+++ JS Back-End-Exam/Book_Тalk_PROFILE/src/config/expressConfig.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+
+const { auth } = require('../middleware/authMiddleware');
+
+function expressConfig(app) {
+    app.use('/static', express.static(path.join(__dirname, '../public')));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(cookieParser());
+    app.use(auth);
+}
+
+module.exports = expressConfig;
