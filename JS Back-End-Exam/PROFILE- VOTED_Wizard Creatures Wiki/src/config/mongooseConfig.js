bbfaf3c05ce352de1717ee18ed01cf623c1c2758
stoyangalchev <galchev98@gmail.com>
2024-02-10 13:16:42 +0200

    DElete

--- JS Back-End-Exam/PROFILE- VOTED_Wizard Creatures Wiki/src/config/mongooseConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const { DB_Connection_String } = require('../constants');
-
-function initDatabase() {
-    return mongoose.connect(DB_Connection_String);
-}
-
-module.exports = initDatabase;
