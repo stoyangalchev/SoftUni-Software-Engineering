ea1038844ef0f16aa09e612170f2d15ced4faff4
stoyangalchev <galchev98@gmail.com>
2024-02-11 09:41:37 +0200

    Create constants.js

---
+++ JS Back-End-Exam/Job Ads _ Search/src/constants.js
@@ -0,0 +1,4 @@
+exports.PORT = 3000;
+exports.DB_Connection_String = "mongodb://localhost:27017/jobs_ads";
+exports.JWT_Secret = "qwertyuiopasdfghjklzxcvbnm123456";
+exports.AUTH_COOKIE_NAME = "auth_jwt";
