fe6a21f00713faf11f959d416cbf4951deebb7b5
stoyangalchev <galchev98@gmail.com>
2024-02-17 10:26:21 +0200

    Create mongooseConfig.js

---
+++ JS Back-End-Exam/Real_Estate_Agency_SEARCH1/src/config/mongooseConfig.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const { DB_Connection_String } = require('../constants');
+
+function initDatabase() {
+    return mongoose.connect(DB_Connection_String);
+}
+
+module.exports = initDatabase;
