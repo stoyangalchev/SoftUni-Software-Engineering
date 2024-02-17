fb071e78b57ad339c4f642179683d8f3e5124d6c
stoyangalchev <galchev98@gmail.com>
2024-02-17 10:25:22 +0200

    Update creaturesServices.js

--- JS Back-End-Exam/Art-Gallery_PROFILE/src/services/creaturesServices.js
+++ JS Back-End-Exam/Art-Gallery_PROFILE/src/services/creaturesServices.js
@@ -5,12 +5,18 @@ const User = require("../models/User");
 exports.getAll = () => Creatures.find().populate("owner").lean();
 
 //Gives the creature by id.
-exports.getOne = (creaturesId) =>
-  Creatures.findById(creaturesId).lean()
-    
+exports.getOne = (creaturesId) => Creatures.findById(creaturesId).lean();
 
 //Creates a new creature.
-exports.create = (creaturesData) => Creatures.create(creaturesData);
+exports.create = async (creaturesData) => {
+  let creatures = new Creatures(creaturesData);
+  let owner = await User.findById(creaturesData.owner);
+  owner.publications.push(creatures._id);
+
+  await owner.save();
+  await creatures.save();
+  return creatures;
+};
 
 //Deletes the creature.
 exports.delete = (creaturesId) => Creatures.findByIdAndDelete(creaturesId);
@@ -20,11 +26,11 @@ exports.updateOne = (creaturesId, creaturesData) =>
   Creatures.findByIdAndUpdate(creaturesId, creaturesData);
 
 //Gives the creature a comment.
-exports.addComment = async (creaturesId, commentData) => {
-  const creatures = await Creatures.findById(creaturesId);
-  creatures.signUpList.push(commentData);
-  return creatures.save();
-};
+// exports.addComment = async (creaturesId, commentData) => {
+//   const creatures = await Creatures.findById(creaturesId);
+//   creatures.signUpList.push(commentData);
+//   return creatures.save();
+// };
 //Gives the owner of the creature.
 exports.findOwner = (userId) => User.findById(userId).lean();
 
@@ -38,8 +44,6 @@ exports.vote = async (creaturesId, commentData) => {
 
   return creatures.save();
 };
-
-//throw new ObjectParameterError(obj, "obj", "Document");??
 
 exports.getMyVoteUp = (userId) => Creatures.find(userId).lean();
 
