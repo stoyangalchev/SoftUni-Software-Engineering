d416e416459055ae14ae94fee73df3832d94f9a9
stoyangalchev <galchev98@gmail.com>
2024-02-18 09:12:05 +0200

    Create authServices.js

---
+++ JS Back-End-Exam/Real_Estate_Agency_SEARCH1/src/services/authServices.js
@@ -0,0 +1,35 @@
+const jwt = require("../utils/jwt");
+const User = require("../models/User");
+const { JWT_Secret } = require("../constants");
+
+exports.register = async (userData) => {
+
+  await User.create(userData);
+
+  
+};
+
+exports.login = async ({ username, password }) => {
+  let user = await User.findOne({ username });
+
+  console.log(user);
+
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+
+  let isValid = await user.validatePassword(password);
+
+  if (!isValid) {
+    throw new Error("Invalid email or password");
+  }
+
+  let payload = {
+    _id: user._id,
+    username: user.username,
+  };
+
+  let token = await jwt.sign(payload, JWT_Secret);
+
+  return token;
+};
