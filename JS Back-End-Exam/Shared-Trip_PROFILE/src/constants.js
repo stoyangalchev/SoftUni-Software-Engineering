617218448052f8467ef3355e1b5d77f67d704830
stoyangalchev <galchev98@gmail.com>
2024-02-28 08:32:31 +0200

    Create constants.js

---
+++ JS Back-End-Exam/Shared-Trip_PROFILE/src/constants.js
@@ -0,0 +1,4 @@
+exports.PORT = 3000;
+exports.DB_Connection_String = "mongodb://localhost:27017/shared-trip";
+exports.JWT_Secret = 'qwertyuiopasdfghjklzxcvbnm123456';
+exports.AUTH_COOKIE_NAME = 'auth_jwt';
