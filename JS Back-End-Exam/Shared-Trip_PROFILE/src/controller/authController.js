2ef9010ab435b0e4366f5fb2f13ed17a7f501483
stoyangalchev <galchev98@gmail.com>
2024-02-28 08:32:35 +0200

    Create authController.js

---
+++ JS Back-End-Exam/Shared-Trip_PROFILE/src/controller/authController.js
@@ -0,0 +1,85 @@
+const router = require("express").Router();
+const authServices = require("../services/authServices");
+const { isGuest, isAuth } = require("../middleware/authMiddleware");
+const { AUTH_COOKIE_NAME } = require("../constants");
+
+router.get("/login", isGuest, (req, res) => {
+  res.render("auth/login");
+});
+
+router.post("/login", isGuest, async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    let token = await authServices.login({
+      email,
+      password,
+    });
+    res.cookie(AUTH_COOKIE_NAME, token);
+    res.redirect("/");
+  } catch (error) {
+    res.render("auth/login", { error: error.message });
+  }
+});
+
+router.get("/register", isGuest, (req, res) => {
+  res.render("auth/register");
+});
+
+router.post("/register", isGuest, async (req, res) => {
+  const { email, password, gender, rePass } = req.body;
+
+  if (password !== rePass) {
+    res.locals.error = "Passwords do not match!";
+    return res.render("auth/register");
+  }
+
+  try {
+    await authServices.register({
+      email,
+      password,
+      gender,
+    });
+
+    let token = await authServices.login({
+      email,
+      password,
+    });
+    res.cookie(AUTH_COOKIE_NAME, token);
+    res.redirect("/");
+  } catch (error) {
+    res.render("auth/register", { error: getErrorMessage(error) });
+  }
+});
+
+
+function getErrorMessage(error) {
+  console.log(error);
+
+  if (
+    error.name == "MongooseError" ||
+    error.name == "Error" ||
+    error.code == "ValidationError"
+  ) {
+    let usernameError = error.keyValue.name;
+    let emailError = error.keyValue.username;
+
+    return usernameError || emailError
+      ? "Username or email is already taken!"
+      : error.message;
+  }
+  let errorsArr = Object.keys(error.errors);
+
+  if (errorsArr.length > 0) {
+    return error.errors[errorsArr[0]];
+  } else {
+    return error.message;
+  }
+}
+
+
+router.get("/logout", isAuth, (req, res) => {
+  res.clearCookie(AUTH_COOKIE_NAME);
+  res.redirect("/");
+});
+
+module.exports = router;
