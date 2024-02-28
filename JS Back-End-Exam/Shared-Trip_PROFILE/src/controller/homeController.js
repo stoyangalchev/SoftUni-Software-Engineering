07ec2e5fc9b69e8ef942da8f9f575347077acce3
stoyangalchev <galchev98@gmail.com>
2024-02-28 08:32:28 +0200

    Create homeController.js

---
+++ JS Back-End-Exam/Shared-Trip_PROFILE/src/controller/homeController.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const { log } = require("util");
+const { isAuth } = require("../middleware/authMiddleware");
+const creaturesServices = require("../services/creaturesServices");
+
+router.get("/", async (req, res) => {
+  let creatures = await creaturesServices.getAll();
+
+  res.render("home", { creatures });
+});
+
+// router.get("/profile", isAuth, async (req, res) => {
+//   let userId = req.user._id;
+
+//   let creatures = await creaturesServices.getMyCreatedPost(userId);
+//   let owner = await creaturesServices.findOwner(userId);
+//   console.log(creatures);
+
+//   res.render("my-posts", {
+//     creatures,
+//     owner,
+//     creaturesCount: creatures.length,
+//   });
+// });
+router.get("/profile", isAuth, async (req, res) => {
+  const userId = req.user._id;
+  let signUp = await creaturesServices.getMySignUp(userId);
+  
+  let filteredSignup = signUp.filter((x) => x.signUpList.length != 0);
+
+  let created = await creaturesServices.getMyCreatedCourse(userId);
+
+  res.render("profile", { filteredSignup, created });
+});
+module.exports = router;
