775e363470c18bf4ed0f08d3ea2494546904e81d
stoyangalchev <galchev98@gmail.com>
2024-03-02 12:38:23 +0200

    Create constants.js

---
+++ JS Back-End-Exam/Second-Hand-Electronics_MPA _SEARCH_/src/constants.js
@@ -0,0 +1,5 @@
+exports.PORT = 3000;
+exports.DB_Connection_String =
+  "mongodb://localhost:27017/second-hand-electronics";
+exports.JWT_Secret = 'qwertyuiopasdfghjklzxcvbnm123456';
+exports.AUTH_COOKIE_NAME = 'auth_jwt';
