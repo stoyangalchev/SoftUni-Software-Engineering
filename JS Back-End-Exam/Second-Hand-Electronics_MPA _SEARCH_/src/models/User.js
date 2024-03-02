dc026f123ebac2a05be2c91cad3dca0f46ef27cf
stoyangalchev <galchev98@gmail.com>
2024-03-02 12:37:48 +0200

    Create User.js

---
+++ JS Back-End-Exam/Second-Hand-Electronics_MPA _SEARCH_/src/models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const SALT_ROUNDS = 10;
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: [true, "Email is required!"],
+    unique: true,
+    minLength: 10,
+  },
+  username: {
+    type: String,
+    required: [true, "Username is required!"],
+    unique: true,
+    minLength: 3,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required!"],
+    minLength: 3,
+  },
+});
+
+userSchema.pre("save", function (next) {
+  return bcrypt.hash(this.password, SALT_ROUNDS).then((hash) => {
+    this.password = hash;
+
+    return next();
+  });
+});
+
+userSchema.method("validatePassword", function (password) {
+  return bcrypt.compare(password, this.password);
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
