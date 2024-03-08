b8c27b8c0e574ac7e87acd3ede558adb9687fa36
stoyangalchev <galchev98@gmail.com>
2024-03-08 19:51:07 +0200

    delete

--- Angular/DI, RxJS, Services/playground-app/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic()
-  // .bootstrapModule(AppModule, { ngZone: 'noop' })
-  .bootstrapModule(AppModule)
-  .catch((err) => console.error(err));
