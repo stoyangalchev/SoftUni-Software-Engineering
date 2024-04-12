066542f47a9cd22a62b7977a1fc7cf6eb1ef09b3
stoyangalchev <galchev98@gmail.com>
2024-04-12 21:05:23 +0300

    Create genericBoxString.js

---
+++ TypeScript /4.DPAndGenerics/Problem-04/dist/genericBoxString.js
@@ -0,0 +1,17 @@
+"use strict";
+class Box {
+    data;
+    constructor(data) {
+        this.data = data;
+    }
+    toString() {
+        return `${this.data} is of type ${typeof this.data}`;
+    }
+}
+const box1 = new Box("Hello");
+console.log(box1.toString());
+const box2 = new Box(20);
+console.log(box2.toString());
+const box3 = new Box(true);
+console.log(box3.toString());
+//# sourceMappingURL=genericBoxString.js.map
