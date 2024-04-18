525aac6c0b3db3a7c100a0d2fb8ef43323ab21e7
stoyangalchev <galchev98@gmail.com>
2024-04-18 09:16:07 +0300

    Create deliveries.ts

---
+++ TypeScript /5.NamespaceAndModules/Problem-05/src/deliveries/deliveries.ts
@@ -0,0 +1,8 @@
+export namespace FoodAndBeverages {
+  export interface Delivery {
+    newCustomer(customerName: string, visited: boolean): void;
+
+    visitCustomer(customerName: string): string;
+    showCustomers(): string;
+  }
+}
