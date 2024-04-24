e93a12a23f928f3aa6e732f6f642ead226df6797
stoyangalchev <galchev98@gmail.com>
2024-04-24 16:18:07 +0300

    Update App.jsx

--- React/React Learning Demo/src/App.jsx
+++ React/React Learning Demo/src/App.jsx
@@ -2,13 +2,11 @@ import "./App.css";
 import MovieList from "./components/movieList/MovieList";
 import Timer from "./components/timer/Timer";
 import Spinner from "./components/spinner/Spinner";
+import movies from "./movies";
 
 function App() {
-  const movies = [
-    { title: "The Godfather", cast: ["Stoyan", "Stanislava"] },
-    { title: "The Dark Knight", cast: ["Stoyan", "Veni"] },
-    { title: "Pulp Fiction", cast: ["Tacho", "Stanislava"] },
-  ];
+
+
 
   return (
     <div>
