f83278e1558bae5ffd09ca1fd0f4967670d44d5a
stoyangalchev <galchev98@gmail.com>
2024-04-24 16:18:15 +0300

    Create movies.js

---
+++ React/React Learning Demo/src/movies.js
@@ -0,0 +1,7 @@
+const movies = [
+  { title: "The Godfather", cast: ["Stoyan", "Stanislavaaaaa"] },
+  { title: "The Dark Knight", cast: ["Stoyan", "Veni"] },
+  { title: "Pulp Fiction", cast: ["Tacho", "Stanislava"] },
+];
+
+export default movies;
