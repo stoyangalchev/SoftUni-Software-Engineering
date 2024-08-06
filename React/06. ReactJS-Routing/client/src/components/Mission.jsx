62d22fb3eefa6025407751417b29285a60b5b0d2
stoyangalchev <galchev98@gmail.com>
2024-08-06 17:08:02 +0300

    Create Mission.jsx

---
+++ React/06. ReactJS-Routing/client/src/components/Mission.jsx
@@ -0,0 +1,9 @@
+export default function Mission() {
+    return (
+        <>
+            <h1>Our Mission</h1>
+            
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim, eaque delectus. Ratione, nobis? Magni, earum nobis. Corporis iusto beatae obcaecati dignissimos ipsam quia harum sequi, illum repudiandae perferendis rem laborum!</p>
+        </>
+    );
+}
