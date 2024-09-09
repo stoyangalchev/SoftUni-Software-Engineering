fd2f7140b7faa4f2561a7d7042edefd761c462f9
stoyangalchev <galchev98@gmail.com>
2024-09-09 07:53:52 +0300

    Update mongooseConfig.js

--- JS Back-End-Exam/WildLife_Profile/src/config/mongooseConfig.js
+++ JS Back-End-Exam/WildLife_Profile/src/config/mongooseConfig.js
@@ -1,9 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const { DB_Connection_String } = require('../constants');
+require("dotenv").config();
+
+const DB_Connection_String = process.env.DB_CONNECTION_STRING;
 
 function initDatabase() {
-    return mongoose.connect(DB_Connection_String);
+  return mongoose.connect(DB_Connection_String);
 }
 
 module.exports = initDatabase;
